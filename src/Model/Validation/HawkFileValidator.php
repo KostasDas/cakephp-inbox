d20e060a93e293d61648ae718a338641583dbf04
kostas <k.t.daskalopoulos@gmail.com>
2018-05-19 21:46:44 +0300

    configuring test environment for folders

---
+++ src/Model/Validation/HawkFileValidator.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kostas
+ * Date: 19/5/2018
+ * Time: 9:38 μμ
+ */
+
+namespace App\Model\Validation;
+
+
+use Cake\Validation\Validator;
+
+class HawkFileValidator extends Validator
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+}
